5ab7f2e4863069851b64ccb32aa5305b63b073fd
dongzhe <dongzhe9303@gmail.com>
2018-09-12 17:04:23 +0800

    更新

--- utils.js
+++ utils.js
@@ -2,9 +2,9 @@
  * 是否在微信中打开
  * @returns {boolean}
  */
-window.isInWeixinApp = function () {
+function isInWeixinApp() {
     return /MicroMessenger/.test(navigator.userAgent);
-};
+}
 
 /**
  * 字符串转义和反转义
@@ -28,16 +28,26 @@ String.prototype.html = function (encode) {
  * @returns {params}
  */
 function getQueryFromURL(url) {
-    url = url || 'http://donzhe93.com/s?a=b#rd';
+    url = url || '';
     var tmp = url.split('?'),
         query = (tmp[1] || "").split('#')[0].split('&'),
         params = {};
     for (var i = 0; i < query.length; i++) {
-        var arg = query[i].split('=');
-        params[arg[0]] = arg[1];
-    }
-    if (params['pass_ticket']) {
-        params['pass_ticket'] = encodeURIComponent(params['pass_ticket'].html(false).html(false).replace(/\s/g, "+"));
+        var queryitem = query[i];
+        if (queryitem !== '') {
+            var arg = query[i].split('=');
+            params[arg[0]] = arg[1];
+        }
     }
     return params;
+}
+
+/**
+ * 验证请求中Referer是否合法
+ * @param referer => req.headers.referer
+ * @param host    => req.headers.host
+ * @returns {boolean}
+ */
+function isCertifiedRequset(referer, host) {
+    return referer === undefined || referer.match(/(https?:\/\/)([\w+\.]+(:[0-9]+)?)\/.+/)[2] === host;
 }
